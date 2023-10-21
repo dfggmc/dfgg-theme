58177f25aa27dfc2329b8b13442b921f49b81196
xcccx <118364173+xcccx-cn@users.noreply.github.com>
2023-10-21 22:48:33 +0800

    动态生成标题

--- public/static/js/IndexScript.js
+++ public/static/js/IndexScript.js
@@ -1,4 +1,15 @@
 console.log("dfgg服务器");
 console.log("version: 1.0.0");
 console.log("Website by: github.com/OLIMINATOR");
+// 获取页面中的所有 <h1> 元素
+var h1Elements = document.getElementsByTagName("h1");
+// 如果存在至少一个 <h1> 元素
+if (h1Elements.length > 0) {
+    // 获取第一个 <h1> 元素的内容
+    var firstH1Element = h1Elements[0];
+    var title = firstH1Element.innerText;
+
+    // 设置页面标题为第一个 <h1> 元素的内容
+    document.title = title;
+}
 
