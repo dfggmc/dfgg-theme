9485a28a4fbb1d6c75fe8a8808e4d34dceedf098
xcccx <118364173+xcccx-cn@users.noreply.github.com>
2023-10-21 22:17:49 +0800

    添加sitemap路由规则

--- public/static/data/router_data.php
+++ public/static/data/router_data.php
@@ -1,12 +1,14 @@
 <?php
+$theme_url = get_theme_url();
 $routes = array(
-    "" => get_theme_url() . "index.php",
-    "/join" => get_theme_url() . "join.php",
-    "/about" => get_theme_url() . "about.php",
-    "/page/plan" => get_theme_url() . "page/plan.php",
-    "/page/SponsorshipList" => get_theme_url() . "page/SponsorshipList.php",
-    "/page/stae" => get_theme_url() . "page/stae/index.php",
-    "/page/game" => get_theme_url() . "page/game.php",
-    "/page/map" => get_theme_url() . "page/map.php",
-    "/game/GoogleDinosaur" => get_theme_url() . "game/GoogleDinosaur/index.php",
+    "" => $theme_url . "index.php",
+    "/join" => $theme_url . "join.php",
+    "/about" => $theme_url . "about.php",
+    "/page/plan" => $theme_url . "page/plan.php",
+    "/page/SponsorshipList" => $theme_url . "page/SponsorshipList.php",
+    "/page/stae" => $theme_url . "page/stae/index.php",
+    "/page/game" => $theme_url . "page/game.php",
+    "/page/map" => $theme_url . "page/map.php",
+    "/game/GoogleDinosaur" => $theme_url . "game/GoogleDinosaur/index.php",
+    "/sitemap.xml" => $theme_url . "sitemap.php"
 );
