92a4eb0d36410b7540747e54e9f43ac8def3822b
xcccx <118364173+xcccx-cn@users.noreply.github.com>
2023-10-22 16:59:55 +0800

    Add files via upload

--- public/static/data/router_data.php
+++ public/static/data/router_data.php
@@ -4,11 +4,9 @@ $routes = array(
     "" => $theme_url . "index.php",
     "/join" => $theme_url . "join.php",
     "/about" => $theme_url . "about.php",
-    "/page/plan" => $theme_url . "page/plan.php",
-    "/page/SponsorshipList" => $theme_url . "page/SponsorshipList.php",
     "/page/stae" => $theme_url . "page/stae/index.php",
     "/page/game" => $theme_url . "page/game.php",
-    "/page/map" => $theme_url . "page/map.php",
     "/game/GoogleDinosaur" => $theme_url . "game/GoogleDinosaur/index.php",
-    "/sitemap.xml" => $theme_url . "sitemap.php"
+    "/sitemap.xml" => $theme_url . "sitemap.php",
+    "/page/(.+)" => $theme_url . "page/page.php",
 );
